849e1a7cff0fdcf51260553f57a08bf10e9cf1b3
Md Sadique Inam <sadique@wiseboxs.com>
2024-01-13 10:42:41 +0530

    Improve fetchServers function in Server.kt and adjust dependencies
    Added functionality for fetchServers method within the Server class and reconfigured the HttpClient in the Module class. New features include fetching servers and handling responses based on status code, whereas HttpClient adjustments now include content negotiation settings and default request specifics. Also updated dependencies in libs.versions.toml to incorporate required changes.

--- server/src/main/kotlin/mdsadiqueinam/github/io/pdns/Module.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/pdns/Module.kt
@@ -2,7 +2,12 @@ package mdsadiqueinam.github.io.pdns
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.resources.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import models.pdns.Error
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
@@ -10,9 +15,20 @@ import org.koin.core.annotation.Single
 @Module
 @ComponentScan
 class PDNSModule {
-
     @Single
     fun client() = HttpClient(CIO) {
         install(Resources)
+        install(ContentNegotiation) {
+            json()
+        }
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTP
+                host = "localhost"
+                port = 8080
+            }
+        }
     }
 }
+
+class PDNSServerException(error: Error) : Throwable(error.error)
--- server/src/main/kotlin/mdsadiqueinam/github/io/pdns/services/Server.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/pdns/services/Server.kt
@@ -1,11 +1,27 @@
 package mdsadiqueinam.github.io.pdns.services
 
 import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.resources.*
+import mdsadiqueinam.github.io.pdns.PDNSServerException
+import models.pdns.Error
+import models.pdns.Server
 import org.koin.core.annotation.Single
+import recources.pdns.Servers
 
 @Single
-class Server(private val client: HttpClient) {
-    fun start() {
-        println("Starting server...")
+class ServerService(private val client: HttpClient) {
+    suspend fun fetchServers(): Result<List<Server>> = kotlin.runCatching {
+        val response = client.get(Servers())
+        return when (response.status.value) {
+            in 200..299 -> {
+                val body = response.body<List<Server>>()
+                Result.success(body)
+            }
+            else -> {
+                val body = response.body<Error>()
+                Result.failure(PDNSServerException(body))
+            }
+        }
     }
 }
