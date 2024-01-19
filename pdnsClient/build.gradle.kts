a0dcd343a17b7170c813349450155311366a3700
Md Sadique Inam <sadique@wiseboxs.com>
2024-01-19 08:02:37 +0530

    Update kotlin build configuration and dependency versions
    Introduced Kotlin/JS and Kotlin/Native configurations in the build.gradle.kts of the pdnsClient project. The build file now includes configuration for different OS and architecture combinations and has specific dependencies for common, jvm, wasm, js, and native modules. The versions of the dependencies have been updated in the libs.versions.toml file.

--- pdnsClient/build.gradle.kts
+++ pdnsClient/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.serialization)
@@ -5,14 +7,31 @@ plugins {
 
 kotlin {
     jvm()
-    applyDefaultHierarchyTemplate()
+    @OptIn(ExperimentalWasmDsl::class) wasmJs {
+        browser()
+        nodejs()
+    }
+    js(IR) {
+        browser()
+        nodejs()
+    }
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    when {
+        hostOs == "Mac OS X" && !isArm64 -> macosX64()
+        hostOs == "Mac OS X" && isArm64 -> macosArm64()
+        hostOs == "Linux" && !isArm64 -> linuxX64()
+        hostOs == "Linux" && isArm64 -> linuxArm64()
+        hostOs == "Windows" -> mingwX64()
+        else -> throw GradleException("Host OS \"$hostOs\" is not supported in Kotlin/Native.")
+    }
+
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
             implementation(libs.bundles.ktor.client)
             implementation(libs.kotlinx.serialization.core)
             implementation(libs.kotlinx.coroutines.core)
-            implementation(libs.ktor.client.cio)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -21,6 +40,19 @@ kotlin {
         jvmMain.dependencies {
             // put your JVM dependencies here
             implementation(libs.logback)
+            implementation(libs.ktor.client.cio)
+        }
+        nativeMain.dependencies {
+            implementation(libs.ktor.client.cio)
+        }
+        jsMain.dependencies {
+            // put your JS dependencies here
+            implementation(libs.ktor.client.js)
+        }
+        val wasmJsMain by getting {
+            dependencies {
+                implementation(libs.bundles.ktor.client.wasm)
+            }
         }
     }
 }
