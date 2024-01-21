61820e2311722c077d2f09e3849caf63efbe4354
Md Sadique Inam <sadique@wiseboxs.com>
2024-01-21 12:06:07 +0530

    Refactor Zone and add CreateZone classes
    Introduced `CreateZone` class for zone creation, and significantly simplified the `Zone` class by removing redundant properties. Additionally, reduced the redundancy in `RRSet`, `Record`, and `Comment` classes by removing unnecessary property documentation.

--- pdnsClient/src/commonMain/kotlin/models/Zone.kt
+++ pdnsClient/src/commonMain/kotlin/models/Zone.kt
@@ -6,8 +6,6 @@ import kotlinx.serialization.Serializable
 /**
  * Enumeration representing different kinds of zones.
  * Each ZoneKind has a corresponding type.
- *
- * @property type The type of the ZoneKind.
  */
 @Serializable
 enum class ZoneKind {
@@ -25,6 +23,43 @@ enum class SOAEditApi {
     EPOCH,
     @SerialName("") OFF
 }
+
+/**
+ * Represents a zone to be created.
+ *
+ * @property name Name of the zone (e.g. 'example.com.') MUST have a trailing dot
+ * @property type Set to 'Zone'
+ * @property kind Zone kind, one of [ZoneKind]
+ * @property nameservers Simple list of nameserver names, including the trailing dot.
+ *                      Not required for slave zones.
+ * @property masters List of IP addresses configured as a master for this zone ('Slave' type zones only)
+ * @property dnssec Whether this zone is DNSSEC signed (inferred from presigned being true XOR presence of at least one cryptokey with active being true)
+ * @property nsec3narrow Whether the zone uses NSEC3 narrow
+ * @property soaEditApi The SOA-EDIT-API metadata item
+ * @property apiRectify Whether the zone will be rectified on data changes via the API
+ * @property zone MAY contain a BIND-style zone file when creating a zone
+ * @property catalog The catalog this zone is a member of
+ * @property account MAY be set. Its value is defined by local policy
+ * @property masterTsigKeyIds The id of the TSIG keys used for master operation in this zone
+ * @property slaveTsigKeyIds The id of the TSIG keys used for slave operation in this zone
+ */
+@Serializable
+data class CreateZone(
+    val name: String,
+    val type: String = "Zone",
+    val kind: ZoneKind = ZoneKind.NATIVE,
+    val nameservers: List<String> = emptyList(),
+    val masters: List<String> = emptyList(),
+    val dnssec: Boolean = true,
+    val nsec3narrow: Boolean = false,
+    @SerialName("soa_edit_api") val soaEditApi: SOAEditApi = SOAEditApi.DEFAULT,
+    @SerialName("api_rectify") val apiRectify: Boolean = false,
+    val zone: String? = null,
+    val catalog: String? = null,
+    val account: String? = null,
+    @SerialName("master_tsig_key_ids") val masterTsigKeyIds: List<String>? = null,
+    @SerialName("slave_tsig_key_ids") val slaveTsigKeyIds: List<String>? = null,
+)
 
 /**
  * Represents a DNS zone.
@@ -60,162 +95,29 @@ enum class SOAEditApi {
  */
 @Serializable
 data class Zone(
-    /**
-     * The application should not interpret opaque zone id (string), assigned by the server.
-     * Guaranteed to be safe for embedding in URLs.
-     */
     val id: String,
-
-    /**
-     * Name of the zone (e.g. 'example.com.') MUST have a trailing dot
-     */
     val name: String,
-
-    /**
-     *  Set to 'Zone'
-     */
-    val type: String = "Zone",
-
-    /**
-     * API endpoint for this zone
-     */
     val url: String,
-
-    /**
-     * Zone kind, one of [ZoneKind]
-     */
     val kind: ZoneKind,
-
-    /**
-     * RRSets in this zone (for zones/{zone_id} endpoint only; omitted during GET on the …/zones list endpoint)
-     */
     val rrsets: List<RRSet>? = null,
-
-    /**
-     *  The SOA serial number
-     */
     val serial: Int,
-
-    /**
-     * The SOA serial notifications have been sent out for
-     */
     @SerialName("notified_serial") val notifiedSerial: Int? = null,
-
-    /**
-     * The SOA serial as seen in query responses.
-     * Calculated using the SOA-EDIT metadata, default-soa-edit and default-soa-edit-signed settings
-     */
     @SerialName("edited_serial") val editedSerial: Int,
-
-    /**
-     * List of IP addresses configured as a master for this zone ('Slave' type zones only)
-     */
     val masters: List<String>,
-
-    /**
-     * Whether this zone is DNSSEC signed
-     * (inferred from presigned being true XOR presence of at least one cryptokey with active being true)
-     */
     val dnssec: Boolean,
-
-    /**
-     * The NSEC3PARAM record
-     */
     val nsec3param: String? = null,
-
-    /**
-     * Whether the zone uses NSEC3 narrow
-     */
     val nsec3narrow: Boolean? = null,
-
-    /**
-     * Whether the zone is pre-signed
-     */
     val presigned: Boolean? = null,
-
-    /**
-     * The SOA-EDIT metadata item
-     */
     @SerialName("soa_edit") val soaEdit: String? = null,
-
-    /**
-     * The SOA-EDIT-API metadata item
-     */
     @SerialName("soa_edit_api") val soaEditApi: SOAEditApi? = null,
-
-    /**
-     * Whether the zone will be rectified on data changes via the API
-     */
     @SerialName("api_rectify") val apiRectify: Boolean? = null,
-
-    /**
-     * MAY contain a BIND-style zone file when creating a zone
-     */
     val zone: String? = null,
-
-    /**
-     * The catalog this zone is a member of
-     */
     val catalog: String? = null,
-
-    /**
-     * MAY be set. Its value is defined by local policy
-     */
     val account: String,
-
-    /**
-     * MAY be sent in client bodies during creation, and MUST NOT be sent by the server.
-     * Simple list of nameserver names, including the trailing dot.
-     * Not required for slave zones.
-     */
     val nameservers: List<String>? = null,
-
-    /**
-     * The id of the TSIG keys used for master operation in this zone
-     */
     @SerialName("master_tsig_key_ids") val masterTsigKeyIds: List<String>? = null,
-
-    /**
-     * The id of the TSIG keys used for slave operation in this zone
-     */
     @SerialName("slave_tsig_key_ids") val slaveTsigKeyIds: List<String>? = null,
-) {
-    constructor(
-        name: String,
-        kind: ZoneKind = ZoneKind.NATIVE,
-        account: String = "",
-        nameservers: List<String> = emptyList(),
-        masters: List<String> = emptyList(),
-        dnssec: Boolean = true,
-        soaEditApi: SOAEditApi = SOAEditApi.DEFAULT,
-        apiRectify: Boolean = false,
-        zone: String? = null,
-        catalog: String? = null,
-        masterTsigKeyIds: List<String>? = null,
-        slaveTsigKeyIds: List<String>? = null,
-    ) : this(
-        id = "",
-        name = name,
-        type = "Zone",
-        url = "",
-        kind = kind,
-        serial = 0,
-        notifiedSerial = null,
-        editedSerial = 0,
-        masters = masters,
-        dnssec = dnssec,
-        presigned = null,
-        soaEdit = "",
-        soaEditApi = soaEditApi,
-        apiRectify = apiRectify,
-        zone = zone,
-        catalog = catalog,
-        account = account,
-        nameservers = nameservers,
-        masterTsigKeyIds = masterTsigKeyIds,
-        slaveTsigKeyIds = slaveTsigKeyIds,
-    )
-}
+)
 
 @Serializable
 data class PatchRRSets(
@@ -294,47 +196,11 @@ enum class ChangeType {
  */
 @Serializable
 data class RRSet(
-    /**
-     * Name for a record set (e.g. 'www.powerdns.com.')
-     */
     val name: String,
-
-    /**
-     * The Type of this record (e.g. 'A', 'PTR', 'MX') MUST be uppercase
-     */
     val type: RRSetType,
-
-    /**
-     * DNS TTL of the records, in seconds. MUST NOT be included when changetype is set to 'DELETE'.
-     */
     val ttl: Int,
-
-    /**
-     * MUST be added when updating the RRSet.
-     * Must be REPLACE or DELETE.
-     * With DELETE, all existing RRs matching name and type will be deleted, including all comments.
-     * With REPLACE: when records are present, all existing RRs matching name and type will be deleted,
-     * and then new records given in records will be created.
-     * If no records are left, any existing comments will be deleted as well.
-     * When comments are present, all existing comments for the RRs matching name and type will be deleted,
-     * and then new comments given in comments will be created.
-     */
     val changetype: ChangeType? = null,
-
-    /**
-     * All records in this RRSet.
-     * When updating Records, this is the list of new records (replacing the old ones).
-     * Must be empty when changetype is set to DELETE.
-     * An empty list results in deletion of all records (and comments).
-     */
     val records: List<Record>,
-
-    /**
-     * List of Comment.
-     * Must be empty when changetype is set to DELETE.
-     * An empty list results in deletion of all comments.
-     * Modified_at is optional and defaults to the current server time.
-     */
     val comments: List<Comment>,
 )
 
@@ -346,14 +212,7 @@ data class RRSet(
  */
 @Serializable
 data class Record(
-    /**
-     * The content of this record
-     */
     val content: String,
-
-    /**
-     * Whether this record is disabled. When unset, the record is not disabled
-     */
     val disabled: Boolean,
 )
 
@@ -366,19 +225,8 @@ data class Record(
  */
 @Serializable
 data class Comment(
-    /**
-     * The actual comment
-     */
     val content: String,
-
-    /**
-     * Name of an account that added the comment
-     */
     val account: String,
-
-    /**
-     * Timestamp of the last change to the comment
-     */
     @SerialName("modified_at") val modifiedAt: Int
 )
 
@@ -390,13 +238,6 @@ data class Comment(
  */
 @Serializable
 data class Metadata(
-    /**
-     * Name of the metadata
-     */
     val kind: String,
-
-    /**
-     * Array with all values for this metadata kind.
-     */
     val metadata: List<String>
 )
