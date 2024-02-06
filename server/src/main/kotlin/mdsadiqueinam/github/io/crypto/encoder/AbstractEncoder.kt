97fe1a392677bffa839947025278d7cf42586dcc
Md Sadique Inam <sadique@wiseboxs.com>
2024-02-06 16:22:37 +0530

    Implement DelegatingPasswordEncoder and enhance encoding utilities
    Introduced the DelegatingPasswordEncoder class which delegates password encoding based on a prefixed identifier. This provides flexibility in choosing the encoding mechanism. Alongside this, enhanced the AbstractEncoder class by adding relevant methods and implemented the EncodingUtils class for array operations, improving overall crypto functionalities.

--- server/src/main/kotlin/mdsadiqueinam/github/io/crypto/encoder/AbstractEncoder.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/crypto/encoder/AbstractEncoder.kt
@@ -1,4 +1,37 @@
 package mdsadiqueinam.github.io.crypto.encoder
 
-abstract class AbstractEncoder {
+import mdsadiqueinam.github.io.crypto.keygen.BytesKeyGenerator
+import mdsadiqueinam.github.io.crypto.keygen.KeyGenerator
+import mdsadiqueinam.github.io.crypto.util.EncodingUtils
+import org.bouncycastle.util.encoders.Hex
+import java.security.MessageDigest
+
+
+abstract class AbstractEncoder protected constructor() : Encoder {
+    private val saltGenerator = KeyGenerator.secureRandom()
+
+    override fun encode(rawPassword: CharSequence): String {
+        val salt = saltGenerator.generateKey()
+        val encoded = encodeAndConcatenate(rawPassword, salt)
+        return String(Hex.encode(encoded))
+    }
+
+    override fun matches(rawPassword: CharSequence, encodedPassword: String): Boolean {
+        val digested = Hex.decode(encodedPassword)
+        val salt: ByteArray = EncodingUtils.subArray(digested, 0, saltGenerator.keyLength)
+        return matches(digested, encodeAndConcatenate(rawPassword, salt))
+    }
+
+    protected abstract fun encode(rawPassword: CharSequence, salt: ByteArray): ByteArray
+
+    protected fun encodeAndConcatenate(rawPassword: CharSequence, salt: ByteArray): ByteArray {
+        return EncodingUtils.concatenate(salt, encode(rawPassword, salt))
+    }
+
+    /**
+     * Constant time comparison to prevent against timing attacks.
+     */
+    protected fun matches(expected: ByteArray, actual: ByteArray): Boolean {
+        return MessageDigest.isEqual(expected, actual)
+    }
 }
