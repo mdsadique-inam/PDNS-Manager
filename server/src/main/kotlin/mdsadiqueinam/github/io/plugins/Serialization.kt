4146d474f0708048fe2640b14a3bc2ea64e82a67
Md Sadique Inam <sadique@wiseboxs.com>
2024-03-07 10:55:35 +0530

    Add Protobuf serialization and update ktor version
    Protobuf serialization has been added as an option in application serialization configuration. The version of ktor has been updated from "2.3.8" to "2.3.9". 'AuthenticationRepository' was removed from 'DI.kt' to improve dependency management. A new module, pdnsClientModule, was added to `RepositoryModule.kt` to handle connections to `http://localhost:8081/api/v1/`.

--- server/src/main/kotlin/mdsadiqueinam/github/io/plugins/Serialization.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/plugins/Serialization.kt
@@ -1,14 +1,18 @@
 package mdsadiqueinam.github.io.plugins
 
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.serialization.kotlinx.protobuf.*
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.resources.Resources
+import kotlinx.serialization.ExperimentalSerializationApi
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
+        protobuf()
     }
     install(Resources)
 }
--- server/src/main/kotlin/mdsadiqueinam/github/io/repositories/RepositoryModule.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/repositories/RepositoryModule.kt
@@ -1,13 +1,35 @@
 package mdsadiqueinam.github.io.repositories
 
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import mdsadiqueinam.github.io.database.services.UserService
 import mdsadiqueinam.github.io.models.JWTConfig
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
+import org.koin.dsl.module
+import org.koin.ksp.generated.module
 import org.springframework.security.crypto.password.PasswordEncoder
+import services.ZoneService
 
-@Module
+
+val pdnsClientModule = module {
+    val client = PDNSClient.createHttpClient {
+        defaultRequest {
+            url("http://localhost:8081/api/v1/")
+            header("X-API-Key", "secret")
+            contentType(ContentType.Application.Json)
+        }
+    }
+    single { ZoneService(client) }
+}
+
+@Module()
 class RepositoryModule(private val jwtConfig: JWTConfig) {
+
+    init {
+        this.module.includes(pdnsClientModule)
+    }
 
     @Single
     fun authenticationRepository(userService: UserService, encoder: PasswordEncoder) = AuthenticationRepository(jwtConfig, userService, encoder)
