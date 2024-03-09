f06129d345e9f8e9254649e48bce03c468a552ac
Md Sadique Inam <sadique@wiseboxs.com>
2024-03-09 16:23:43 +0530

    Implement GET and POST methods in Zone route file
    The code modifications added GET and POST methods to the Zone route, incorporating the principal and serverId as parameters. This allows better interaction with the ZoneRepository. Some commented code remains for possible future use.

--- server/src/main/kotlin/mdsadiqueinam/github/io/routes/zones.kt
+++ server/src/main/kotlin/mdsadiqueinam/github/io/routes/zones.kt
@@ -1,9 +1,26 @@
 package mdsadiqueinam.github.io.routes
 
-import io.ktor.server.routing.*
+import io.ktor.server.application.call
+import io.ktor.server.auth.principal
+import io.ktor.server.resources.get
+import io.ktor.server.resources.post
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import mdsadiqueinam.github.io.plugins.JWTUserPrincipal
 import mdsadiqueinam.github.io.repositories.ZoneRepository
 import org.koin.ktor.ext.inject
+import resources.Zones
 
 fun Route.zones() {
     val zoneRepository by inject<ZoneRepository>()
+    get<Zones> {
+        val principal = call.principal<JWTUserPrincipal>()
+        val zones = zoneRepository.fetchZones(principal!!.user, it.parent.serverId, it.zone, it.dnssec)
+        call.respond(zones)
+    }
+    post<Zones> {
+        val principal = call.principal<JWTUserPrincipal>()
+//        val zone = zoneRepository.createZone(principal!!.user, it.parent.serverId, it.body, it.rrsets)
+//        call.respond(zone)
+    }
 }
